aa73fafd38562a34b5fdf459bfcc0212400aa414
ronaldo-os <ronaldo@paralejas.com>
2025-05-19 17:44:48 +0800

    - Initial Branch Commit - Created the basic structure of the rails app tax_form_hub - Added javascript and bootstrap. - Added active storage for file uploads.

---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+// Entry point for the build script in your package.json
+import * as bootstrap from "bootstrap"
+import "bootstrap"
+import "../stylesheets/application.bootstrap"  // or wherever your Bootstrap CSS is imported
