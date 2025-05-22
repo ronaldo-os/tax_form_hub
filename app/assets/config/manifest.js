3d033cf7be30a7f89599a323aad82369c0a4723c
ronaldo-os <rcp@meteorsoftwareph.com>
2025-05-22 12:55:27 +0800

    - Email now sending to the existing account. - Added bootstrap on change password page. - All repetitive js of signup,login and change password has been added into one js file.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link application.js
 //= link application.css
+//= link devise_password_toggle.js
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "devise_password_toggle", to: "devise_password_toggle.js"
