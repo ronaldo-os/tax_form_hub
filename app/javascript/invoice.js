2b7bf675e2f0ff8ed2bac7167689a4d9ac348981
ronaldo-os <rcp@meteorsoftwareph.com>
2025-07-02 14:02:37 +0800

    - Moved to long_select_options/options.js for it to be used globally - Added country and discount options

--- app/javascript/invoice.js
+++ app/javascript/invoice.js
@@ -1,3 +1,5 @@
+import { COUNTRY_OPTIONS, DISCOUNT_OPTIONS } from "./long_select_options/options";
+
 if (window.location.pathname.includes("/invoices")) {
 
     $(document).ready(function () {
@@ -359,34 +361,6 @@ if (window.location.pathname.includes("/invoices")) {
 
     $(document).ready(function () {
 
-    const COUNTRY_OPTIONS = [
-        "", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina",
-        "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados",
-        "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana",
-        "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon",
-        "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)",
-        "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark",
-        "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
-        "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia",
-        "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti",
-        "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy",
-        "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan",
-        "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-        "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania",
-        "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique",
-        "Myanmar (Burma)", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger",
-        "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine State",
-        "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania",
-        "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa",
-        "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone",
-        "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea",
-        "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan",
-        "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia",
-        "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom",
-        "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam",
-        "Yemen", "Zambia", "Zimbabwe"
-    ];
-
     const $select = $('#initial_delivery_details_country');
 
     if ($select.length) {
@@ -397,26 +371,6 @@ if (window.location.pathname.includes("/invoices")) {
         }));
       });
     }
-    
-    const DISCOUNT_OPTIONS =  [
-        "Choose reason code",
-        "Bank Charges",
-        "Customs Duties",
-        "Repair Costs",
-        "Attorney Fees",
-        "Taxes",
-        "Late Delivery",
-        "Freight Costs",
-        "Reason Unknown",
-        "Price Change",
-        "Early payment allowance adjustment",
-        "Quantity Discount",
-        "Pricing Discount",
-        "Volume Discount",
-        "Agreed Discount",
-        "Expediting fee",
-        "Currency exchange differences"
-    ];
 
     let lineIndex = 1;
 
