5e61af5e8e675c08ac65807766e25604e7e1a435
ronaldo-os <rcp@meteorsoftwareph.com>
2025-07-08 16:54:24 +0800

    - Move company profile to user profile. - Removed all the old fields and created a new form for company profile similar to tradeshift - Removed all old companies js. - Added adaptive labels for company id and tax id type select options.

--- app/javascript/companies.js
+++ app/javascript/companies.js
@@ -1,60 +1,12 @@
 if (window.location.pathname.includes("/companies")) {
     $(document).ready(function () {
-        $('#companies-table').DataTable({
-            responsive: true,
-            pageLength: 10,
-            columnDefs: [
-                { orderable: false, targets: -1 }
-            ]
-        });
+        function updateLabel(selectId, labelId, defaultText) {
+            $(selectId).on('change', function () {
+                const text = $(this).val() ? $(this).find('option:selected').text() + ' Number' : defaultText;
+                $(labelId).text(text);
+            });
+        }
+        updateLabel('#company_id_type', '#company_id_number_label', '');
+        updateLabel('#tax_id_type', '#tax_id_number_label', '');
     });
-
-    // Handle Add New Company button
-    $('#add-new-company-btn').on('click', function () {
-      $('#companyModalLabel').text("Add New Company");
-      $('#company-form').attr('action', '/companies');
-      $('#form-method-field').val('post');
-
-      // Clear form fields
-      $('#company_name, #company_number, #tax_number, #country, #address_line1, #address_line2, #address_line3, #zip_city').val('');
-    });
-
-    // Edit button handler
-    $('.edit-company-btn').on('click', function () {
-        const $btn = $(this);
-
-        $('#companyModalLabel').text("Edit Company");
-        $('#company-form').attr('action', $btn.data('url'));
-        $('#form-method-field').val('patch');
-
-        $('#company_name').val($btn.data('name'));
-        $('#company_number').val($btn.data('company-number'));
-        $('#tax_number').val($btn.data('tax-number'));
-        $('#country').val($btn.data('country'));
-        $('#address_line1').val($btn.data('address-line1'));
-        $('#address_line2').val($btn.data('address-line2'));
-        $('#zip_code').val($btn.data('zip-code'));
-        $('#city').val($btn.data('city'));
-        });
-
-    // View button handler
-    $('.view-company-btn').on('click', function () {
-        const $btn = $(this);
-
-        $('#view_name').text($btn.data('name'));
-        $('#view_company_number').text($btn.data('company-number'));
-        $('#view_tax_number').text($btn.data('tax-number'));
-        $('#view_address_line1').text($btn.data('address-line1'));
-        $('#view_address_line2').text($btn.data('address-line2'));
-
-        const zip = $btn.data('zip-code');
-        const city = $btn.data('city');
-        $('#view_zip_code_city').text(`${zip} ${city}`);
-        $('#view_country').text($btn.data('country'));
-
-    });
-
-
-
-
 }
