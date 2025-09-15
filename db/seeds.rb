e3f744b9cd5f20f07fb0bf901b0253c5d9dc6ff4
Ronaldo Paralejas Jr. <rcp@meteorsoftwareph.com>
2025-09-15 18:34:13 +0800

    - on rails db seed added a overwrite user when ran more than twice and the values are still the same

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
-superadmin = User.find_or_create_by!(email: ENV['SUPERADMIN_EMAIL']) do |user|
-  user.password = ENV['SUPERADMIN_PASSWORD']
-  user.password_confirmation = ENV['SUPERADMIN_PASSWORD']
-  user.role = 'superadmin'
-  user.confirmed_at = Time.current if user.respond_to?(:confirmed_at)
+superadmin = User.find_or_initialize_by(email: ENV['SUPERADMIN_EMAIL'])
+
+superadmin.password              = ENV['SUPERADMIN_PASSWORD']
+superadmin.password_confirmation = ENV['SUPERADMIN_PASSWORD']
+superadmin.role                  = 'superadmin'
+superadmin.confirmed_at          = Time.current if superadmin.respond_to?(:confirmed_at)
+superadmin.save!
+
+if superadmin.previously_new_record?
+  puts "✅ Superadmin created: #{superadmin.email}"
+else
+  puts "♻️ Superadmin updated: #{superadmin.email}"
 end
 
 company_name = ENV['SUPERADMIN_COMPANY_NAME']
-
 existing_company = Company.find_by(name: company_name)
 
 if existing_company
@@ -17,5 +23,3 @@ else
   superadmin.update!(company: company)
   puts "✅ Company created and linked to Superadmin: #{company.name}"
 end
-
-puts "Superadmin created: #{superadmin.email}"
